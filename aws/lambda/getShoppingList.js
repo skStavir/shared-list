09a00d14072cf8b9e1009201a02bd32d9e82acc7
santkk <kksant@gmail.com>
2020-07-11 21:58:13 +0530

    fix the extra type info of dynamo db

--- aws/lambda/getShoppingList.js
+++ aws/lambda/getShoppingList.js
@@ -1,23 +1,21 @@
 const AWS = require("aws-sdk");
 
 AWS.config.update({region: 'ap-south-1'});
-const ddb = new AWS.DynamoDB({apiVersion: '2012-08-10'});
+const ddb = new AWS.DynamoDB.DocumentClient({apiVersion: '2012-08-10'});
 
 
 const params = {
   TableName: 'shoppinglist',
   Key: {
-    'id': {
-      S: '1'
-    },
+    'id': '1',
   }
 };
 exports.handler = async (event) => {
-  let shoppinglist = await ddb.getItem(params, function (err, data) {
+  let shoppinglist = await ddb.get(params, function (err, data) {
     if (err) {
       return 'Error';
     } else {
-      return JSON.stringify(data.Item);
+      return AWS.DynamoDB.Converter.unmarshall(data.Item);
     }
   }).promise();
 
