9bc7fb051e9f625e7a3e5da440fc44de78c89a46
santkk <kksant@gmail.com>
2020-07-12 01:32:18 +0530

    support initializing new shopping list

--- aws/lambda/getShoppingList.js
+++ aws/lambda/getShoppingList.js
@@ -1,5 +1,6 @@
 const AWS = require("aws-sdk");
 
+
 AWS.config.update({region: 'ap-south-1'});
 const dynamoDb = new AWS.DynamoDB.DocumentClient({apiVersion: '2012-08-10'});
 
@@ -8,8 +9,34 @@ const params = {
 };
 
 exports.handler = async (event) => {
+  let id;
+
+  if (typeof event.pathParameters != 'undefined' && event.pathParameters) {
+    id = event.pathParameters.id;
+  }
+
+  if (typeof id === 'undefined' || !id) {
+    //TODO use uuid
+    id = Math.random().toString().substring(2);
+    let newParams = {
+      TableName: 'shoppinglist',
+      Item: {
+        'id': id,
+        'items': ['rice', 'pen'],
+        'cart': ['atta', 'pencil'],
+      }
+    };
+    await dynamoDb.put(newParams, function (err, data) {
+      if (err) {
+        console.log("Error in initializing data", err);
+      } else {
+        console.log("Successfully initialized data", data);
+      }
+    }).promise();
+  }
+
   params.Key = {
-    'id': event.pathParameters.id
+    'id': id
   };
   let shoppinglist = await dynamoDb.get(params, function (err, data) {
     if (err) {
